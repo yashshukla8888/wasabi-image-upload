1aa9b70a48133af0f06e881e4ba59dab343c6db6
yashshukla <yashs018@gmail.com>
2021-02-04 00:11:46 +0530

    upload to s3 and display

--- asset/ckeditor/ck_upload.php
+++ asset/ckeditor/ck_upload.php
@@ -7,29 +7,32 @@ require './../../vendor/autoload.php';
 
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
+$bucketName = "dadaji-bhartiya-sanskruti-chair";
 
-
-$bucketName = 'ckeditor';
-// $bucketName = "rao-learning-buket";
-$IAM_KEY = 'W7LDE7KT64ISVKOY040W';
-$IAM_SECRET = 'FJK9KoiB5p89GlFqteA3p8OAdpOpSlw73ShaoWZX';
-
+// AWS DETAILS
+// $CKEditorFuncNum = $_GET['CKEditorFuncNum']; 
 // $IAM_KEY = 'AKIAXDGTPD32F4XXZ3SD';
 // $IAM_SECRET = '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';
 
-$s3 = S3Client::factory(array(
-    'endpoint' => 's3.wasabisys.com',
-    'region' => 'us-west-1',
-    'version' => '2006-03-01',
-    'use_path_style_endpoint' => true,
-    'credentials' => [
-        'key' => 'W7LDE7KT64ISVKOY040W',
-        'secret' => 'FJK9KoiB5p89GlFqteA3p8OAdpOpSlw73ShaoWZX',
-    ]
-));
+// WASABI DETAILS
+$bucketName = 'test-rao';
+$IAM_KEY= 'D1LJCO5WO10TNJH4P1YP';
+$IAM_SECRET= 'ZbFJouHEB1bn92HbJjhQNyUCd1UT2H1dOEsIBI4D';
+
+$sepext = explode('.', strtolower($_FILES['upload']['name'])); 
+$type = end($sepext);    /** gets extension **/ 
+
+$imgset = array( 
+    'maxsize' => 2000, 
+    'maxwidth' => 1024, 
+    'maxheight' => 800, 
+    'minwidth' => 10, 
+    'minheight' => 10, 
+    'type' => array('bmp', 'gif', 'jpg', 'jpeg', 'png'), 
+); 
 
 // $s3 = S3Client::factory(array(
-//     'region' => 'us-east-2',
+//     'region' => 'us-west-2',
 //     'version' => '2006-03-01',
 //     'use_path_style_endpoint' => true,
 //     'credentials' => [
@@ -38,25 +41,36 @@ $s3 = S3Client::factory(array(
 //     ]
 // ));
 
+$s3 = S3Client::factory(array(
+    'endpoint' => 'http://s3.wasabisys.com',
+    'region' => 'us-west-1',
+    'version' => 'latest',
+    // 'profile' => 'wasabi',
+    'use_path_style_endpoint' => true,
+    'credentials' => [
+        'key' => $IAM_KEY,
+        'secret' => $IAM_SECRET,
+    ]
+));
+
 
 try {
-    $objects = $s3->listObjects([
-        'Bucket' => $bucketName
+    
+    
+    $result = $s3->putObject([
+        'Bucket' => $bucketName,
+        'Key'    => $_FILES['upload']['name'],
+        'ACL'    => 'public-read',
+        'SourceFile' => $_FILES['upload']['tmp_name'],
     ]);
-    $objectArray = array();
-    foreach ($objects as $object) {
-        $objectArray[] = $object;
-    }
+    $msg = $type .' successfully uploaded: \\n- Size: '. number_format($_FILES['upload']['size']/1024, 2, '.', '') .' KB'; 
 
-    print_r($objectArray);
-    // $result = $s3->putObject([
-    //     'Bucket' => $bucketName,
-    //     'Key'    => "yash_shukla.text",
-    //     'Body'   => 'Yash CHeck',
-    //     'ACL'    => 'public-read'
-    // ]);
-
-    // echo $result['ObjectURL'] . PHP_EOL;
+    $url = $result['ObjectURL'];
+    $re = in_array($type, $imgset['type']) ? "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>":'<script>var cke_ob = window.parent.CKEDITOR; for(var ckid in cke_ob.instances) { if(cke_ob.instances[ckid].focusManager.hasFocus) break;} cke_ob.instances[ckid].insertHtml(\' \', \'unfiltered_html\'); alert("'. $msg .'"); var dialog = cke_ob.dialog.getCurrent();dialog.hide();</script>'; 
 } catch (S3Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
+    $re = $e->getMessage() . PHP_EOL;
 }
+
+
+@header('Content-type: text/html; charset=utf-8'); 
+echo $re;
